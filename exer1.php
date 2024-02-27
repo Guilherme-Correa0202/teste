b1a48259cb0dc6584022b8dfe5f9a0b1345d52de
Guilherme-Correa0202 <silva.correa@aluno.ifsp.edu.br>
2024-02-27 11:28:23 -0300

    senha e eail

--- exer1.php
+++ exer1.php
@@ -1,8 +1,11 @@
 <?php
-$num1 = $_GET['num1'];
-$num2 = $_GET['num2'];
-
-$num3 = $num1 + $num2;
-
-echo $num3;
+$nome = $_GET['nome'];
+$email = $_GET['email'];
+$senha = $_GET['senha'];
+if($nome === "Guilherme" && $email === "guicorrea@gmail.com" && $senha === "02022007"){
+echo "<h1>Logado com sucesso</h1>". "<BR>" ."Nome: $nome ". "<BR>" . "Email: $email". "<BR>" . "Senha $senha";}
+else{
+    echo "<h1>SENHA, NOME OU EMAIL incorretos</h1>";
+    echo  "<a href='index.html'>VOLTAR</a>";
+}
 ?>
